2ac4b17862f516caeba6ad94349f6e8cd4f29a0f
abuArsalan <arsalanshafi911@gmail.com>
2025-05-05 15:31:56 +0530

    adedd input field

--- frontend/components/Input.jsx
+++ frontend/components/Input.jsx
@@ -1,11 +1,14 @@
-
+import { FontAwesomeIcon } from "react-fontawesome";
+import { fa }  from "@fortawesome/fontawesome-free-solid";
 
 
 function Input(){
     return(
-        <div className="">
-            <input placeholder="enter your message"></input>
-            <button></button>
+        <div className="w-[50%] mx-auto">
+            <input placeholder="enter your message" className="w-[50%] p-5 bg-gray-500 text-amber-300 rounded-2xl font-bold"></input>
+            <button>
+                <FontAwesomeIcon icon={fa-paper-plane}></FontAwesomeIcon>
+            </button>
         </div>
     )
 }
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import Input from "../components/Input"
 function App() {
   return (
     <>
-      <div className="w-[50%] bg-yellow-100 mx-auto mt-[100px] h-[450px] rounded-3xl shadow-2xl"></div>
+      <div className="w-[50%] bg-yellow-100 mx-auto mt-[100px] mb-0 h-[450px] rounded-3xl shadow-2xl"></div>
       <Input />
       </>
 
