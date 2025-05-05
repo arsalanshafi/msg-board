90232691708e66c357e255614a3ec5213f014d59
abuArsalan <arsalanshafi911@gmail.com>
2025-05-05 16:11:32 +0530

    created states for messages

--- frontend/components/Input.jsx
+++ frontend/components/Input.jsx
@@ -1,27 +1,33 @@
 import { FaPaperPlane } from "react-icons/fa"
+import { useState } from "react"
 
 
-function clickHandler(){
-    console.log("pressed")
-}
+
 
 function Input(){
+
+    const [message,setMessage] = useState("");
+
+    function clickHandler(msg){
+        return (msg) => {setMessage(msg.target.value);
+            console.log(message)
+        }
+    }
+
     return(
         <div className="w-[50%] mx-auto flex">
-            <input placeholder="enter your message" className="w-[90%] p-5 bg-gray-500 text-amber-300  rounded-bl-2xl font-bold"></input>
+            <input placeholder="enter your message" className="w-[90%] p-5 bg-gray-500 text-amber-300  rounded-bl-2xl font-bold" onChange={clickHandler(event.target.value)}></input>
             <label htmlFor="sub"><FaPaperPlane size={70} className="bg-gray-500 p-4  rounded-br-2xl "/></label>
-            <input type="submit" id="sub" className="hidden" onClick={clickHandler}></input>
-            
-
+            <input type="submit" id="sub" className="hidden"></input>
         </div>
     )
 }
 
 
-function Icon({icon}){
-    <div className=" bg-gray-500 rounded-br-2 rounded-tr-2xl">
-        {icon}
-    </div>
-}
+// function Icon({icon}){
+//     <div className=" bg-gray-500 rounded-br-2 rounded-tr-2xl">
+//         {icon}
+//     </div>
+// }
 
 export default Input;
