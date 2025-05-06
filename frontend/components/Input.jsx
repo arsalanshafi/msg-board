46b113d05373c62c05676aae3be885c18d8c56de
abuArsalan <arsalanshafi911@gmail.com>
2025-05-06 08:34:18 +0530

    modified json object

--- frontend/components/Input.jsx
+++ frontend/components/Input.jsx
@@ -4,39 +4,50 @@ import { useState } from "react"
 
 
 
-function Input(){
+function Input() {
 
-    const [message,setMessage] = useState("");
+    const [message, setMessage] = useState("");
 
-    function clickHandler(msg){
-        return (msg) => {setMessage(msg.target.value);
-            console.log(message)
+    function clickHandler(msg) {
+        return (msg) => {
+            setMessage(msg.target.value);
+            // console.log(message)
         }
     }
 
-    async function postMessage(){
+    async function postMessage() {
+        if (!message) {
+            console.log("message is empty");
+            return
+        }
+        console.log("sending message")
 
-        var payload= {"body":"this is from react"};
-        var data = new FormData();
-        data.append("json",JSON.stringify(payload));
+        var payload = { "body": `${message}` };
+        // var data = new FormData();
+        // data.append("json",JSON.stringify(payload));
         // data.append("json",payload);
 
-
-        const result = await fetch("http://localhost:5000/msg/api/upload",{
-            method:"POST",
-            headers:{
-                "content-type":"application/json"
-            },
-            body: data,
+        try {
+            const result = await fetch("http://localhost:5000/msg/api/upload", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json;charset=utf-8"
+                },
+                body: JSON.stringify(payload),
+            }
+            )
+            console.log(result);
         }
-    )
-    console.log(result);
+        catch (e) {
+            console.log(e)
+        }
+
     }
 
-    return(
+    return (
         <div className="w-[50%] mx-auto flex">
-            <input placeholder="enter your message" className="w-[90%] p-5 bg-gray-500 text-amber-300  rounded-bl-2xl font-bold" onChange={clickHandler(event.target.value)}></input>
-            <label htmlFor="sub"><FaPaperPlane size={70} className="bg-gray-500 p-4  rounded-br-2xl "/></label>
+            <input placeholder="enter your message" className="w-[90%] p-5 bg-gray-500 text-amber-300  rounded-bl-2xl font-bold" onChange={clickHandler(event)}></input>
+            <label htmlFor="sub"><FaPaperPlane size={70} className="bg-gray-500 p-4  rounded-br-2xl " /></label>
             <input type="submit" id="sub" className="hidden" onClick={postMessage}></input>
         </div>
     )
